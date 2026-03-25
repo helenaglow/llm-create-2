ee39d71603e14184b3e36d79e0646c081b405085
Helena Glowacki <113125436+helenaglow@users.noreply.github.com>
2026-03-25 14:23:38 -0400

    one passage

--- src/consts/passages.tsx
+++ src/consts/passages.tsx
@@ -1,46 +1,46 @@
 import type { Passage } from "../types";
 
 export const Passages: Passage[] = [
+  // {
+  //   id: "1",
+  //   text: `The wind swept in soft big breaths down from the moor and was strange with a wild clear scented sweetness. Colin kept lifting his thin chest to draw it in, and his big eyes looked as if it were they which were listening - listening, instead of his ears. "There are so many sounds of singing and humming and calling out," he said. "What is that scent the puffs of wind bring?" "It's gorse on th' moor that's openin' out" answered Dickon. "Eh! th' bees are at it wonderful to-day." Not a human creature was to be caught sight of in the paths they took. In fact every gardener or gardener's lad had been witched away. But they wound in and out among the shrubbery and out and round the fountain beds, following their carefully planned route for the mere mysterious pleasure of it. But when at last they turned into the Long Walk by the ivied walls the excited sense of an approaching thrill made them, for some curious reason they could not have explained, begin to speak in whispers. "This is it" breathed Mary. "This is where I used to walk up and down and wonder and wonder".`,
+  //   author: "Frances Hodson Burnett",
+  //   title: "The Secret Garden",
+  // },
+  // {
+  //   id: "2",
+  //   text: "Two days later they arrived on the Common. The doctors had arranged to have Sylvia in the cottage, and had got permission from a farmer for the girls to camp in the field of a farm nearby. They had fixed that they should come every day for their middle-day meal at the cottage, for which Pauline was to pay a pound a week. For breakfast, tea, and supper they were to cook and cater for themselves. Perhaps it was because they were not expecting a holiday at all, or perhaps because it was Pauline’s holiday which she had paid for, but there was not one second which they did not find perfect. They took turns to cook, and it was lovely waking in the mornings to hear the cows mooing, and the cocks crowing, and to turn over and prod the day’s cook to get up and deal with breakfast. The palliasses stuffed with straw they found gloriously comfortable, and it was the height of luxury to lie on them sniffing the first smoke of the fire through the open flap of the tent before jumping up to race down to the stream in a bathing dress for a wash before breakfast. ",
+  //   author: "Noel Streatfeild",
+  //   title: "Ballet Shoes",
+  // },
+  // {
+  //   id: "3",
+  //   text: "The food varied a lot with who was cooking and catering. Eggs were the easiest things for breakfast, because they bought them from the farm; but for supper they had all sorts of things — sausages when Petrova was in charge, and two courses at least from Pauline. When Posy was catering, a great deal of cake got into the menu. The weather was not too good, but the farmer lent them a large barn for wet days, in which they practised every morning, wet or shine, as they had, of course, their ballet shoes with them; here, when it rained, they played a new and glorious kind of hide-and-seek. It consisted in the hiders burying themselves in the straw, which the nervous seeker had to prod. If the hider could grab any part of the seeker, she won; but if the seeker could see a movement in the hay, and lay a hand on the place, and say ‘One of you is here’, then she won. The game was such a thrilling one that when it was wet Sylvia and the two doctors usually came and played too.",
+  //   author: "Noel Streatfeild",
+  //   title: "Ballet Shoes",
+  // },
+  // {
+  //   id: "4",
+  //   text: `"That may make me feel badly tomorrow, Josie," laughed Anne, "but just now I honestly feel that as long as I know the violets are coming out all purple down in the hollow below Green Gables and that little ferns are poking their heads up in Lovers' Lane, it's not a great deal of difference whether I win the Avery or not. I've done my best and I begin to understand what is meant by the 'joy of the strife.' Next to trying and winning, the best thing is trying and failing. Girls, don't talk about exams! Look at that arch of pale green sky over those houses and picture to yourself what it must look like over the purply-dark beech-woods back of Avonlea." "What are you going to wear for commencement, Jane?" asked Ruby practically. Jane and Josie both answered at once and the chatter drifted into a side eddy of fashions. But Anne, with her elbows on the window sill, her soft cheek laid against her clasped hands, and her eyes filled with visions, looked out unheedingly across city roof and spire to that glorious dome of sunset sky and wove her dreams of a possible future from the golden tissue of youth's own optimism.`,
+  //   author: "Lucy Maud Montgomery",
+  //   title: "Anne of Green Gables",
+  // },
+  // {
+  //   id: "5",
+  //   text: `A woman who was so hard on herself and others could be sweet and smiling with Clara and at times, by extension, with Blanca. Only with Clara did she allow herself the luxury of giving in to her overwhelming desire to serve and be loved; with her, however slyly, she was able to express the secret, most delicate yearnings of her soul. e long years of solitude and unhappiness had distilled her emotions and purified her feelings down to a few terrible, magnificent passions, which possessed her totally. She had no gift for small perturbations, mean-spirited resentments, concealed envies, works of charity, faded endearments, ordinary friendly politeness, or day-to-day acts of kindness. She was one of those people who are born for the greatness of a single love, for exaggerated hatred, for apocalyptic vengeance, and for the most sublime forms of heroism, but she was unable to shape her fate to the dimensions of her amorous vocation, so it was lived out as something flat and gray trapped between her mother’s sickroom walls, wretched tenements, and the tortured confessions with which this large, opulent, hot-blooded woman—made for maternity, abundance, action, and ardor—was consuming herself.`,
+  //   author: "Isabel Allende",
+  //   title: "The House of Spirits",
+  // },
   {
     id: "1",
-    text: `The wind swept in soft big breaths down from the moor and was strange with a wild clear scented sweetness. Colin kept lifting his thin chest to draw it in, and his big eyes looked as if it were they which were listening - listening, instead of his ears. "There are so many sounds of singing and humming and calling out," he said. "What is that scent the puffs of wind bring?" "It's gorse on th' moor that's openin' out" answered Dickon. "Eh! th' bees are at it wonderful to-day." Not a human creature was to be caught sight of in the paths they took. In fact every gardener or gardener's lad had been witched away. But they wound in and out among the shrubbery and out and round the fountain beds, following their carefully planned route for the mere mysterious pleasure of it. But when at last they turned into the Long Walk by the ivied walls the excited sense of an approaching thrill made them, for some curious reason they could not have explained, begin to speak in whispers. "This is it" breathed Mary. "This is where I used to walk up and down and wonder and wonder".`,
-    author: "Frances Hodson Burnett",
-    title: "The Secret Garden",
-  },
-  {
-    id: "2",
-    text: "Two days later they arrived on the Common. The doctors had arranged to have Sylvia in the cottage, and had got permission from a farmer for the girls to camp in the field of a farm nearby. They had fixed that they should come every day for their middle-day meal at the cottage, for which Pauline was to pay a pound a week. For breakfast, tea, and supper they were to cook and cater for themselves. Perhaps it was because they were not expecting a holiday at all, or perhaps because it was Pauline’s holiday which she had paid for, but there was not one second which they did not find perfect. They took turns to cook, and it was lovely waking in the mornings to hear the cows mooing, and the cocks crowing, and to turn over and prod the day’s cook to get up and deal with breakfast. The palliasses stuffed with straw they found gloriously comfortable, and it was the height of luxury to lie on them sniffing the first smoke of the fire through the open flap of the tent before jumping up to race down to the stream in a bathing dress for a wash before breakfast. ",
-    author: "Noel Streatfeild",
-    title: "Ballet Shoes",
-  },
-  {
-    id: "3",
-    text: "The food varied a lot with who was cooking and catering. Eggs were the easiest things for breakfast, because they bought them from the farm; but for supper they had all sorts of things — sausages when Petrova was in charge, and two courses at least from Pauline. When Posy was catering, a great deal of cake got into the menu. The weather was not too good, but the farmer lent them a large barn for wet days, in which they practised every morning, wet or shine, as they had, of course, their ballet shoes with them; here, when it rained, they played a new and glorious kind of hide-and-seek. It consisted in the hiders burying themselves in the straw, which the nervous seeker had to prod. If the hider could grab any part of the seeker, she won; but if the seeker could see a movement in the hay, and lay a hand on the place, and say ‘One of you is here’, then she won. The game was such a thrilling one that when it was wet Sylvia and the two doctors usually came and played too.",
-    author: "Noel Streatfeild",
-    title: "Ballet Shoes",
-  },
-  {
-    id: "4",
-    text: `"That may make me feel badly tomorrow, Josie," laughed Anne, "but just now I honestly feel that as long as I know the violets are coming out all purple down in the hollow below Green Gables and that little ferns are poking their heads up in Lovers' Lane, it's not a great deal of difference whether I win the Avery or not. I've done my best and I begin to understand what is meant by the 'joy of the strife.' Next to trying and winning, the best thing is trying and failing. Girls, don't talk about exams! Look at that arch of pale green sky over those houses and picture to yourself what it must look like over the purply-dark beech-woods back of Avonlea." "What are you going to wear for commencement, Jane?" asked Ruby practically. Jane and Josie both answered at once and the chatter drifted into a side eddy of fashions. But Anne, with her elbows on the window sill, her soft cheek laid against her clasped hands, and her eyes filled with visions, looked out unheedingly across city roof and spire to that glorious dome of sunset sky and wove her dreams of a possible future from the golden tissue of youth's own optimism.`,
-    author: "Lucy Maud Montgomery",
-    title: "Anne of Green Gables",
-  },
-  {
-    id: "5",
-    text: `A woman who was so hard on herself and others could be sweet and smiling with Clara and at times, by extension, with Blanca. Only with Clara did she allow herself the luxury of giving in to her overwhelming desire to serve and be loved; with her, however slyly, she was able to express the secret, most delicate yearnings of her soul. e long years of solitude and unhappiness had distilled her emotions and purified her feelings down to a few terrible, magnificent passions, which possessed her totally. She had no gift for small perturbations, mean-spirited resentments, concealed envies, works of charity, faded endearments, ordinary friendly politeness, or day-to-day acts of kindness. She was one of those people who are born for the greatness of a single love, for exaggerated hatred, for apocalyptic vengeance, and for the most sublime forms of heroism, but she was unable to shape her fate to the dimensions of her amorous vocation, so it was lived out as something flat and gray trapped between her mother’s sickroom walls, wretched tenements, and the tortured confessions with which this large, opulent, hot-blooded woman—made for maternity, abundance, action, and ardor—was consuming herself.`,
-    author: "Isabel Allende",
-    title: "The House of Spirits",
-  },
-  {
-    id: "6",
     text: `The nights are clear, but suffused with sloth and sullen expectation. But by early June the southwest monsoon breaks and there are three months of wind and water with short spells of sharp, glittering sunshine that thrilled children snatch to play with. The countryside turns an immodest green. Boundaries blur as tapioca fences take root and bloom. Brick walls turn moss green. Pepper vines snake up electric poles. Wild creepers burst through laterite banks and spill across flooded roads. Boats ply in the bazaars. And small fish appear in the puddles that fill the PWD potholes on the highways. It was raining when Rahel came back to Ayemenem. Slanting silver ropes slammed into loose earth, plowing it up like gunfire. The old house on the hill wore its steep, gabled roof pulled over its ears like a low hat. The walls, streaked with moss, had grown soft, and bulged a little with dampness that seeped up from the ground. The wild, overgrown garden was full of the whisper and scurry of small lives. In the undergrowth a rat snake rubbed itself against a glistening stone. Hopeful yellow bullfrogs cruised the scummy pond for mates. A drenched mongoose flashed across the leaf-strewn driveway.`,
     author: "Arundhati Roy",
     title: "The God of Small Things",
   },
-  {
-    id: "7",
-    text: `The house itself looked empty. The doors and windows were locked. The front verandah bare. Unfurnished. But the skyblue Plymouth with chrome tailfins was still parked outside, and inside, Baby Kochamma was still alive. She was Rahel’s baby grandaunt, her grandfather’s younger sister. Her name was really Navomi, Navomi Ipe, but everybody called her Baby. She became Baby Kochamma when she was old enough to be an aunt. Rahel hadn’t come to see her, though. Neither niece nor baby grandaunt labored under any illusions on that account. Rahel had come to see her brother, Estha. They were two-egg twins. “Dizygotic” doctors called them. Born from separate but simultaneously fertilized eggs. Estha—Esthappen was the older by eighteen minutes. They never did look much like each other, Estha and Rahel, and even when they were thin-armed children, flat-chested, wormridden and Elvis Presley-puffed, there was none of the usual “Who is who?” and “Which is which?” from oversmiling relatives or the Syrian Orthodox bishops who frequently visited the Ayemenem House for donations. The confusion lay in a deeper, more secret place.`,
-    author: "Arundhati Roy",
-    title: "The God of Small Things",
-  },
+  // {
+  //   id: "7",
+  //   text: `The house itself looked empty. The doors and windows were locked. The front verandah bare. Unfurnished. But the skyblue Plymouth with chrome tailfins was still parked outside, and inside, Baby Kochamma was still alive. She was Rahel’s baby grandaunt, her grandfather’s younger sister. Her name was really Navomi, Navomi Ipe, but everybody called her Baby. She became Baby Kochamma when she was old enough to be an aunt. Rahel hadn’t come to see her, though. Neither niece nor baby grandaunt labored under any illusions on that account. Rahel had come to see her brother, Estha. They were two-egg twins. “Dizygotic” doctors called them. Born from separate but simultaneously fertilized eggs. Estha—Esthappen was the older by eighteen minutes. They never did look much like each other, Estha and Rahel, and even when they were thin-armed children, flat-chested, wormridden and Elvis Presley-puffed, there was none of the usual “Who is who?” and “Which is which?” from oversmiling relatives or the Syrian Orthodox bishops who frequently visited the Ayemenem House for donations. The confusion lay in a deeper, more secret place.`,
+  //   author: "Arundhati Roy",
+  //   title: "The God of Small Things",
+  // },
 ];
