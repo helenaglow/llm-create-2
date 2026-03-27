20d0d142fd27e0e353d3abf6ee789272ae55c313
Sarah Wang <sarahxp.wang@mail.utoronto.ca>
2026-03-26 20:18:21 -0400

    yeehaw

--- src/components/shared/preventBackBttn.tsx
+++ src/components/shared/preventBackBttn.tsx
@@ -2,7 +2,7 @@ import { useEffect } from "react";
 
 export default function usePreventBack(message: string) {
   useEffect(() => {
-    window.history.pushState(null, "", window.location.pathname);
+    window.history.pushState(null, "", window.location.href);
 
     const handlePopState = () => {
       alert(message);
